884d50e4a286eac458decc528614668acca770c0
jbpratt78 <jbpratt78@gmail.com>
2020-03-16 17:13:48 -0500

    parsing messages into data type

--- bot.hs
+++ bot.hs
@@ -1,19 +1,69 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-import Wuss
+import           Wuss
 
-import GHC.Generics
-import Control.Concurrent (forkIO)
-import Control.Monad (forever, unless, void)
-import qualified Data.Text as T (Text, pack, words, unwords)
--- import Data.Aeson (decode)
-import Network.WebSockets (ClientApp, receiveData, sendClose)
+import           GHC.Generics
+import           Control.Concurrent             ( forkIO )
+import           Control.Monad                  ( forever
+                                                , unless
+                                                , void
+                                                )
+import           Data.Int                       ( Int64 )
+import           Data.List                      ( (!!) )
+import qualified Data.Text                     as T
+                                                ( Text
+                                                , pack
+                                                , unpack
+                                                , words
+                                                , unwords
+                                                , head
+                                                )
+import qualified Data.Text.Lazy                as L
+                                                ( fromStrict )
+import qualified Data.Text.Lazy.Encoding       as E
+                                                ( encodeUtf8 )
+import qualified Data.ByteString.Lazy.Char8    as C
+                                                ( pack
+                                                , unpack
+                                                , ByteString
+                                                )
+import           Data.Aeson                     ( decode
+                                                , FromJSON
+                                                , parseJSON
+                                                , withObject
+                                                , (.:)
+                                                )
+import           Network.WebSockets             ( ClientApp
+                                                , receiveData
+                                                , sendClose
+                                                )
 
-data Message = Message {
+data MessageType = MSG | PRIVMSG | JOIN | QUIT | VIEWERSTATE deriving (Show, Enum, Read)
+
+data ChatMessage = ChatMessage {
       nick :: T.Text
-    , dta :: T.Text
-    } deriving (Generic, Show) 
+    , content :: T.Text
+    , features :: [T.Text]
+    , timestamp :: Data.Int.Int64
+    } deriving (Generic, Show)
+
+
+instance FromJSON ChatMessage where
+  parseJSON = withObject "ChatMessage" $ \v -> do
+    nick      <- v .: "nick"
+    features  <- v .: "features"
+    content   <- v .: "data"
+    timestamp <- v .: "timestamp"
+    return
+      (ChatMessage { nick      = nick
+                   , features  = features
+                   , content   = content
+                   , timestamp = timestamp
+                   }
+      )
+
 
 main :: IO ()
 main = runSecureClient "chat.strims.gg" 443 "/ws" ws
@@ -23,15 +73,25 @@ ws connection = do
   putStrLn "Connected!"
 
   void . forkIO . forever $ do
-      message <- receiveData connection
-      -- decode (Message . Data.Text.unwords $ tail $ Data.Text.words (message :: Data.Text.Text))
-      print (message :: T.Text)
+    message <- receiveData connection
+    let mt =
+          read $ T.unpack $ head $ T.words (message :: T.Text) :: MessageType
+    print $ parseMessage (message :: T.Text)
 
   let loop = do
-          line <- getLine
-          unless (null line) loop
+        line <- getLine
+        unless (null line) loop
 
   loop
 
   sendClose connection (T.pack "Closed")
   putStrLn "Disconnected"
+
+
+parseMessage :: T.Text -> Maybe ChatMessage
+parseMessage msg =
+  decode $ E.encodeUtf8 $ L.fromStrict (parseMessageContent msg) :: Maybe
+      ChatMessage
+
+parseMessageContent :: T.Text -> T.Text
+parseMessageContent = T.pack . unwords . tail . words . T.unpack
